cf4e66ff7ed5ac667fe39163d50fc297e7975ee2
ennodapondatti <clguse2006@gmail.com>
2025-08-09 20:06:49 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+}
+
+export default nextConfig
